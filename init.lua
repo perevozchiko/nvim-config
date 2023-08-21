74ac3410f5f87b0709ccc3ee9d3b0022b9933761
andrei <>
2023-08-21 22:27:43 +0200

    initial config neovim

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- Basic
+require('core.plugins')
+require('core.mappings')
+
+--Plugins
+require('plugins.neotree')
+require('plugins.treesitter')
+require('plugins.hop')
---
+++ lua/core/plugins.lua
@@ -0,0 +1,28 @@
+-- Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+        lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{ 'phaazon/hop.nvim',
+  branch = 'v2', -- optional but strongly recommended
+  config = function()
+    -- you can configure Hop the way you like here; see :h hop-config
+    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+  end},	
+	{"nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+},
+{'nvim-treesitter/nvim-treesitter'},
+})
