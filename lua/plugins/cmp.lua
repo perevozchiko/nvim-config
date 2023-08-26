2df4403431c3b5c3e22503da79d5921e7fa1694c
andrei <>
2023-08-26 14:35:21 +0200

    Change lsp server for cmp plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -63,6 +63,6 @@
   -- Set up lspconfig.
   local capabilities = require('cmp_nvim_lsp').default_capabilities()
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+  require('lspconfig')['clangd'].setup {
     capabilities = capabilities
   }
